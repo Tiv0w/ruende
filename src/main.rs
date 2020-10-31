1fc40f498a76e60561d190030d1070da15a024f8
Tiv0w <t.theomeyer@gmail.com>
2020-10-31 19:34:17 +0100

    :recycle: Clean main

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,36 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:#?}", args);
-
     if args.len() < 3 {
         println!("Usage: ruende FILE DEST");
         std::process::exit(64);
     }
 
-    let x = fs::read(&args[1]).expect("Couldn't read the file.");
+    println!("Text: {}", &args[1]);
 
-    let code16 = lzw::v2::encode(&x);
+    let file = fs::read(&args[1]).expect("Couldn't read the file.");
 
-    let compression_ratio = code16.len() as f32 / x.len() as f32;
+    let code = lzw::v2::encode(&file);
+
+    let compression_ratio = code.len() as f32 / file.len() as f32;
 
     println!(
         "
-SHAKESPEARE:
-
-Text   length: {}
-Code16 length: {}
-Space gain       : {:.1}% ({})
+Text length      : {}
+Code length      : {}
+% of the original: {:.1}% ({})
 Compression ratio: {:.1}% ({})
-Space saving     : {:.1}% ({})
+Space saved      : {:.1}% ({})
 ",
-        x.len(),
-        code16.len(),
+        file.len(),
+        code.len(),
         compression_ratio * 100 as f32,
         compression_ratio,
         100 as f32 / compression_ratio,
         1 as f32 / compression_ratio,
         (1 as f32 - compression_ratio) * 100 as f32,
-        1 as f32 / compression_ratio
+        1 as f32 - compression_ratio
     );
 
-    fs::write(&args[2], code16).expect("Couldn't write to DEST");
+    fs::write(&args[2], code).expect("Couldn't write to DEST");
 }
