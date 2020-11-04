bbba05b0d86cec652ae6161a77fb27856ab12281
Tiv0w <t.theomeyer@gmail.com>
2020-11-04 10:53:50 +0100

    :sparkles: Add CLI possibility to display compression info

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,6 @@ fn main() {
         .author("Tiv0w <t.theomeyer@gmail.com>")
         .about("RUst ENcoder & DEcoder is a general-purpose compression system.")
         .arg(
-            Arg::with_name("decode")
-                .short("d")
-                .long("decode")
-                .help("Tells RUENDE to decode INPUT"),
-        )
-        .arg(
             Arg::with_name("SRC")
                 .help("Sets the source file")
                 .required(true)
@@ -31,12 +25,29 @@ fn main() {
                 .index(2),
         )
         .arg(
+            Arg::with_name("decode")
+                .short("d")
+                .long("decode")
+                .help("Tells RUENDE to decode INPUT"),
+        )
+        .arg(
+            Arg::with_name("info")
+                .short("i")
+                .long("info")
+                .help("Prints compression ratio information"),
+        )
+        .arg(
             Arg::with_name("rle")
                 .short("r")
                 .long("rle")
-                .help("Tells RUENDE to use RLE"),
+                .help("Tells RUENDE to use RLE algorithm"),
         )
         .get_matches();
+
+    if matches.args.len() < 2 {
+        println!("{}", matches.usage());
+        std::process::exit(64);
+    }
 
     let arg_src = matches
         .value_of("SRC")
@@ -45,19 +56,15 @@ fn main() {
         .value_of("DEST")
         .expect("DEST argument cannot be read");
     let arg_decode = matches.is_present("decode");
-
-    if matches.args.len() < 2 {
-        println!("{}", matches.usage());
-        std::process::exit(64);
-    }
+    let arg_info = matches.is_present("info");
 
     let file = fs::read(arg_src).expect("Couldn't read the file.");
 
     if !arg_decode {
         let code = lzw::v2::encode(&file);
-
-        utils::compression::print_compression_ratio(file.len(), code.len());
-
+        if arg_info {
+            utils::compression::print_compression_ratio(file.len(), code.len());
+        }
         fs::write(arg_dest, code).expect("Couldn't write to DEST");
     } else {
         // let _decode = lzw::v2::decode(&code);
