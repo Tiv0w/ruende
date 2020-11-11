cdf826e2bd3a1bff5746d9825900c0b41cd4c4ce
Tiv0w <t.theomeyer@gmail.com>
2020-11-11 19:02:11 +0100

    :sparkles: :tada: THE DECODING IS WORKING!! (v3)

--- src/lzw/v3.rs
+++ src/lzw/v3.rs
@@ -10,10 +10,6 @@ mod utils {
 pub fn decode(data: &Vec<u8>) -> Vec<u8> {
     let mut result: Vec<u8> = Vec::new();
 
-    for idx in 0..data.len() {
-        print!("| {} ", data[idx]);
-    }
-
     let mut initial_dictionary_idx: Vec<Vec<u8>> = Vec::new();
     for ascii_symbol in 0..=0xff {
         initial_dictionary_idx.push(vec![ascii_symbol]);
@@ -24,12 +20,8 @@ pub fn decode(data: &Vec<u8>) -> Vec<u8> {
     let mut reserved_byte_ready = false;
     let mut reserved_byte: u8 = 0;
 
-    println!("\nON EST LA");
-
-    let mut _old_code: u16;
-    let initial_code = utils::couple_of_u8_to_u16((data[0], data[1]));
-    let mut old_vec: Vec<u8> = vec![initial_code as u8];
-    result.push(initial_code as u8);
+    let mut old_code = utils::couple_of_u8_to_u16((data[0], data[1]));
+    result.push(old_code as u8);
 
     for &byte in &data[2..] {
         if dictionary_len == 0xffff {
@@ -38,17 +30,15 @@ pub fn decode(data: &Vec<u8>) -> Vec<u8> {
             dictionary_len = 0x100;
         }
 
-        println!("tour: {} ", dictionary_len - 0x100);
-
         if !reserved_byte_ready {
             reserved_byte = byte;
             reserved_byte_ready = true;
         } else {
-            let code = utils::couple_of_u8_to_u16((reserved_byte, byte));
+            let current_code = utils::couple_of_u8_to_u16((reserved_byte, byte));
+            let mut old_vec: Vec<u8> = dictionary_idx[old_code as usize].clone();
 
-            if code < dictionary_len {
-                println!("Inférieur à dictionary_len: {}", code);
-                let vec: &Vec<u8> = &dictionary_idx[code as usize];
+            if current_code < dictionary_len {
+                let vec: &Vec<u8> = &dictionary_idx[current_code as usize];
                 let vec_first_byte: u8 = vec[0];
 
                 for &byte in vec {
@@ -56,27 +46,26 @@ pub fn decode(data: &Vec<u8>) -> Vec<u8> {
                 }
 
                 old_vec.push(vec_first_byte);
-                dictionary_idx.push(old_vec.clone());
+                dictionary_idx.push(old_vec);
                 dictionary_len += 1;
             } else {
-                println!("Supérieur à dictionary_len: {}", code);
-
                 let old_first_byte: u8 = old_vec[0];
-
                 old_vec.push(old_first_byte);
 
                 for &byte in &old_vec {
                     result.push(byte);
                 }
 
-                dictionary_idx.push(old_vec.clone());
+                dictionary_idx.push(old_vec);
                 dictionary_len += 1;
             }
+
+            old_code = current_code;
             reserved_byte_ready = false;
         }
     }
 
-    println!("RESULT: {}", String::from_utf8(result.clone()).unwrap());
+    // println!("RESULT: {}", String::from_utf8(result.clone()).unwrap());
 
     result
 }
